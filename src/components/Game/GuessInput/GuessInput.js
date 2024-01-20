e7b179c47e4dbc1e4b03e786ee9ffffb066914d6
Arkady Dolina <dolinaa@rfa.org>
2024-01-20 14:15:14 -0500

    Update GuessInput.js

--- src/components/Game/GuessInput/GuessInput.js
+++ src/components/Game/GuessInput/GuessInput.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 
-function GuessInput({ guesses, setGuesses, isDisabled=false }) {
+function GuessInput({ guesses, setGuesses, isDisabled }) {
   const [userGuess, setUserGuess] = useState('');
 
   return (
